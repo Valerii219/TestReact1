d33f0ada8f746d4d883b4e311a7c7613c754bdc7
Valerii219 <valera.arl96@gmail.com>
2024-07-14 14:10:45 -0400

    save

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,25 @@
+export function  Statistics ({title, stats}){
+    return (
+        <section class="statistics">
+  <h2 class="title">{title}</h2>
+
+  <ul class="stat-list">
+    <li key={stats.id} class="item">
+      <span class="label">{stats.label}</span>
+      <span class="percentage">{stats.percentage}</span>
+    </li>
+    <li class="item">
+      <span class="label">.mp3</span>
+      <span class="percentage">14%</span>
+    </li>
+    <li class="item">
+      <span class="label">.pdf</span>
+      <span class="percentage">41%</span>
+    </li>
+    <li class="item">
+      <span class="label">.mp4</span>
+      <span class="percentage">12%</span>
+    </li>
+  </ul>
+</section>
+    )}
